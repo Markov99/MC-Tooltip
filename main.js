49a646600727a69a895475203b3ebce14b02d489
Markov Dmitry <markovdmitry99@ya.ru>
2020-06-05 08:24:57 +0300

    tooltip position changes as it is in mc

--- main.js
+++ main.js
@@ -1,46 +1,33 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", function () {
 
-
-
-    var minetip = document.createElement("div");
-    minetip.className = "minetip";
-    minetip.innerHTML = "test</br>test2";
-
+    var offset = 20;
 
     function handler(event) {
-        // pageX = event.pageX;
-        // pageY = event.pageY;
+
         minetip = event.currentTarget.querySelector("div");
-        // console.log(minetip);
-        // console.log(event.target);
-        // console.log(event.type);
-        // console.log(event.currentTarget.attributes);
-        // console.log(event.currentTarget.attributes["data-minetip-title"].nodeValue);
-        // console.log(event.currentTarget.attributes["data-minetip-text"].nodeValue);
-        if (event.type == "mouseover") {
+        if (event.type == "mousemove" || event.type == "mouseover") {
             minetip.style.display = "block";
-            minetip.style.left = event.pageX+"px";
-            // minetip.style.top = (event.pageY-minetip.offsetHeight/2)+"px";
-            minetip.style.top = event.pageY-20+"px";
+            if (window.innerWidth < event.pageX + offset + minetip.offsetWidth) {
+                minetip.style.left = event.pageX - offset - minetip.offsetWidth + "px";
+            } else {
+                minetip.style.left = event.pageX + offset + "px";
+            }
+            if (window.innerHeight < event.pageY - offset + minetip.offsetHeight) {
+                minetip.style.top = event.pageY - (event.pageY + minetip.offsetHeight - window.innerHeight) + "px";
+            } else {
+                minetip.style.top = event.pageY - offset + "px";
+            }
         } else if (event.type == "mouseout") {
             minetip.style.display = "none";
-        } else if (event.type == "mousemove") {
-            minetip.style.left = event.pageX+"px";
-            // minetip.style.top = (event.pageY-minetip.offsetHeight/2)+"px";
-            minetip.style.top = event.pageY-20+"px";
         }
     }
 
 
     var minetips = document.querySelectorAll("span.minetip");
     for (var i = 0; i < minetips.length; i++) {
-        // console.log(minetips[i]);
-        // event.currentTarget.appendChild(minetip);
         minetips[i].addEventListener("mouseover", handler);
         minetips[i].addEventListener("mouseout", handler);
         minetips[i].addEventListener("mousemove", handler);
     }
-
-
 
 });
