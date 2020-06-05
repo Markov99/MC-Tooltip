36788c5075963fc07416c8e4709f126339cdbdeb
Markov Dmitry <markovdmitry99@ya.ru>
2020-06-05 06:32:00 +0300

    tooltip works. font added

--- main.js
+++ main.js
@@ -2,39 +2,45 @@ window.addEventListener("load", () => {
 
 
 
-var minetip = document.createElement("div");
-minetip.className = "minetip";
+    var minetip = document.createElement("div");
+    minetip.className = "minetip";
+    minetip.innerHTML = "test</br>test2";
 
 
-function handler(event) {
-    // console.log(event.target);
-    // console.log(event.type);
-    // console.log(event.currentTarget.attributes);
-    console.log(event.currentTarget.attributes["data-minetip-title"].nodeValue);
-    console.log(event.currentTarget.attributes["data-minetip-text"].nodeValue);
-    if (event.type == "mouseover") {
-
-    } else if (event.type == "mouseout") {
-        
-    } else if (event.type == "mousemove") {
-
+    function handler(event) {
+        // pageX = event.pageX;
+        // pageY = event.pageY;
+        minetip = event.currentTarget.querySelector("div");
+        // console.log(minetip);
+        // console.log(event.target);
+        // console.log(event.type);
+        // console.log(event.currentTarget.attributes);
+        // console.log(event.currentTarget.attributes["data-minetip-title"].nodeValue);
+        // console.log(event.currentTarget.attributes["data-minetip-text"].nodeValue);
+        if (event.type == "mouseover") {
+            minetip.style.display = "block";
+            minetip.style.left = event.pageX+"px";
+            // minetip.style.top = (event.pageY-minetip.offsetHeight/2)+"px";
+            minetip.style.top = event.pageY-20+"px";
+        } else if (event.type == "mouseout") {
+            minetip.style.display = "none";
+        } else if (event.type == "mousemove") {
+            minetip.style.left = event.pageX+"px";
+            // minetip.style.top = (event.pageY-minetip.offsetHeight/2)+"px";
+            minetip.style.top = event.pageY-20+"px";
+        }
     }
-}
 
 
-var minetips = document.querySelectorAll("span.minetip");
-for (var i=0; i<minetips.length;i++) {
-    // console.log(minetips[i]);
-    minetips[i].addEventListener("mouseover", handler);
-    minetips[i].addEventListener("mouseout", handler);
-    minetips[i].addEventListener("mousemove", handler);
-}
+    var minetips = document.querySelectorAll("span.minetip");
+    for (var i = 0; i < minetips.length; i++) {
+        // console.log(minetips[i]);
+        // event.currentTarget.appendChild(minetip);
+        minetips[i].addEventListener("mouseover", handler);
+        minetips[i].addEventListener("mouseout", handler);
+        minetips[i].addEventListener("mousemove", handler);
+    }
 
 
 
 });
-
-
-
-
-
