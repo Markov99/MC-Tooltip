152603e579c2a4ace9219c8aaaebb3766c855fba
Markov Dmitry <markovdmitry99@ya.ru>
2020-06-05 14:03:46 +0300

    still

--- main.js
+++ main.js
@@ -54,23 +54,63 @@ window.addEventListener("load", function () {
     var i, j;
     var minetip;
     var minetips;
-    var load_config;
     var event_config;
+    var load_config;
     var configs = document.querySelectorAll("span.minetips");
     for (i = 0; i < configs.length; i++) {
         minetips = configs[i].querySelectorAll("span.minetip");
         for (j = 0; j < minetips.length; j++) {
-            load_config = clone_simple_object(default_load_config);
             event_config = clone_simple_object(default_event_config);
-            update_simple_dict(load_config, {
-                
-            });
+            load_config = clone_simple_object(default_load_config);
+            // console.log(event_config);
             update_simple_dict(event_config, {
-
+                Xoffset: parseInt(minetips[j].parentElement.attributes["data-xoffset"].value),
+                Yoffset: parseInt(minetips[j].parentElement.attributes["data-yoffset"].value),
             });
+            // console.log(event_config);
+            console.log('minetips[j].parentElement.attributes["data-xoffset"]:', minetips[j].parentElement.attributes["data-xoffset"].value);
+            console.log('minetips[j].parentElement.attributes["data-yoffset"]:', minetips[j].parentElement.attributes["data-yoffset"].value);
+            console.log(parseInt(minetips[j].parentElement.attributes["data-xoffset"].value));
+            console.log(parseInt(minetips[j].parentElement.attributes["data-yoffset"].value));
+            console.log(typeof minetips[j].parentElement.attributes["data-xoffset"].value);
+            // console.log("minetips[j].parentElement:", minetips[j].parentElement);
+            // console.log("minetips[j].parentElement.attributes:", minetips[j].parentElement.attributes);
+            // console.log('minetips[j].parentElement.attributes["data-xoffset"]:', minetips[j].parentElement.attributes["data-xoffset"]);
+            // console.log('minetips[j].parentElement.attributes["data-yoffset"]:', minetips[j].parentElement.attributes["data-yoffset"]);
+            // console.log("event_config:", event_config);
+            update_simple_dict(load_config, {
+                img_width: "32px",
+                img_height: "32px",
+                img_alt: "*image*",
+            });
+            
             minetips[j].addEventListener("mouseover", eventHandler);
             minetips[j].addEventListener("mouseout", eventHandler);
             minetips[j].addEventListener("mousemove", eventHandler);
         }
     }
 });
+/*
+
+obj1 = {1:2,2:3,dict:"dict",realdict:{1:2}}
+obj2 = {1:5,dict:{6:7,3:0},realdict:{1:2}}
+function update_simple_dict(obj, ...arguments) {
+    for (var i = 1; i < arguments.length; i++) {
+        for (var prop in arguments[i]) {
+            var val = arguments[i][prop];
+            if (typeof val == "object") // this also applies to arrays or null!
+                update_dict(obj[prop], val);
+            else
+                obj[prop] = val;
+        }
+    }
+    return obj;
+}
+
+
+var test1 = update_simple_dict(obj1, obj2)
+console.log(test1, obj1)
+
+*/
+
+
