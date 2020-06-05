c6230f0beb8cf41a2f73b3bdf839065bd7da9f78
Markov Dmitry <markovdmitry99@ya.ru>
2020-06-05 16:59:40 +0300

    broke something else

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 window.addEventListener("load", function () {
 
     function create_handler(config) {
-        config["data-offset-x"] = parseInt(config["data-offset-x"]);
-        config["data-offset-y"] = parseInt(config["data-offset-y"]);
-        let handler = "";
-
-        handler += "var config=" + JSON.stringify(config) + ";" + window.handler_base;
-        console.log(handler);
-        return (event) => {
-            return eval(handler);
+        // config["data-offset-x"] = parseInt(config["data-offset-x"]);
+        // config["data-offset-y"] = parseInt(config["data-offset-y"]);
+        // let handler = "";
+        // console.log(config);
+        // handler += "var config=" + JSON.stringify(config) + ";" + window.handler_base;
+        // console.log(handler);
+        return function (event) {
+            return eval("var config=" + JSON.stringify(config) + ";" + window.handler_base);
         };
     }
 
@@ -45,7 +45,8 @@ window.addEventListener("load", function () {
                     window.config_event[k] = window.default_config_event[k];
                 }
             }
-            console.log(window.config_event);
+            // console.log(attributes);
+            // console.log(window.config_event);
             let handler = create_handler(window.config_event);
             minetips[j].addEventListener("mouseover", handler);
             minetips[j].addEventListener("mouseout", handler);
@@ -58,32 +59,30 @@ window.addEventListener("load", function () {
 /*
 
 function eventHandler(event) {
-        return handler(event, event_config);
-    }
+    return handler(event, event_config);
+}
 
-    function handler(event, config) {
-        minetip = event.currentTarget.querySelector("div");
-        if (event.type == "mousemove" || event.type == "mouseover") {
-            minetip.style.display = "block";
-            if (window.innerWidth < event.pageX + config["data-offset-x"]
-config["data-offset-x"] + minetip.offsetWidth) {
-                minetip.style.left = event.pageX - config["data-offset-x"]
-config["data-offset-x"] - minetip.offsetWidth + "px";
-            } else {
-                minetip.style.left = event.pageX + config["data-offset-x"]
-config["data-offset-x"] + "px";
-            }
-            if (window.innerHeight < event.pageY - config["data-offset-x"]
-config["data-offset-y"] + minetip.offsetHeight) {
-                minetip.style.top = event.pageY - (event.pageY + minetip.offsetHeight - window.innerHeight) + "px";
-            } else {
-                minetip.style.top = event.pageY - config["data-offset-x"]
-config["data-offset-y"] + "px";
-            }
-        } else if (event.type == "mouseout") {
-            minetip.style.display = "none";
+function handler(event, config) {
+    minetip = event.currentTarget.querySelector("div");
+    if (event.type == "mousemove" || event.type == "mouseover") {
+        minetip.style.display = "block";
+        if (window.innerWidth < event.pageX + config["data-offset-x"] config["data-offset-x"] + minetip.offsetWidth) {
+            minetip.style.left = event.pageX - config["data-offset-x"]
+            config["data-offset-x"] - minetip.offsetWidth + "px";
+        } else {
+            minetip.style.left = event.pageX + config["data-offset-x"]
+            config["data-offset-x"] + "px";
         }
+        if (window.innerHeight < event.pageY - config["data-offset-x"] config["data-offset-y"] + minetip.offsetHeight) {
+            minetip.style.top = event.pageY - (event.pageY + minetip.offsetHeight - window.innerHeight) + "px";
+        } else {
+            minetip.style.top = event.pageY - config["data-offset-x"]
+            config["data-offset-y"] + "px";
+        }
+    } else if (event.type == "mouseout") {
+        minetip.style.display = "none";
     }
+}
 
 
 minetip=event.currentTarget.querySelector("div");if(event.type=="mousemove"||event.type=="mouseover"){minetip.style.display="block";if(window.innerWidth<event.pageX+config["data-offset-x"]config["data-offset-x"]+minetip.offsetWidth){minetip.style.left=event.pageX-config["data-offset-x"];config["data-offset-x"]-minetip.offsetWidth+"px"}else{minetip.style.left=event.pageX+config["data-offset-x"];config["data-offset-x"]+"px"}if(window.innerHeight<event.pageY-config["data-offset-x"]config["data-offset-y"]+minetip.offsetHeight){minetip.style.top=event.pageY-(event.pageY+minetip.offsetHeight-window.innerHeight)+"px"}else{minetip.style.top=event.pageY-config["data-offset-x"];config["data-offset-y"]+"px"}}else if(event.type=="mouseout"){minetip.style.display="none"}
@@ -112,3 +111,24 @@ config["data-offset-y"] + "px";
 }
 
     */
+   var config = {};
+
+minetip = event.currentTarget.querySelector("div");
+if (event.type == "mousemove" || event.type == "mouseover") {
+    minetip.style.display = "block";
+    if (window.innerWidth < event.pageX + config["data-offset-x"] config["data-offset-x"] + minetip.offsetWidth) {
+        minetip.style.left = event.pageX - config["data-offset-x"];
+        config["data-offset-x"] - minetip.offsetWidth + "px";
+    } else {
+        minetip.style.left = event.pageX + config["data-offset-x"];
+        config["data-offset-x"] + "px";
+    }
+    if (window.innerHeight < event.pageY - config["data-offset-x"] config["data-offset-y"] + minetip.offsetHeight) {
+        minetip.style.top = event.pageY - (event.pageY + minetip.offsetHeight - window.innerHeight) + "px";
+    } else {
+        minetip.style.top = event.pageY - config["data-offset-x"];
+        config["data-offset-y"] + "px";
+    }
+} else if (event.type == "mouseout") {
+    minetip.style.display = "none";
+}
