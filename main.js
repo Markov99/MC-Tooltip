1961d25615511bf001e16058684fcab745265e76
Markov Dmitry <markovdmitry99@ya.ru>
2020-06-05 09:55:51 +0300

    started making config js

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
 window.addEventListener("load", function () {
 
-    var minetip;
-    var Xoffset = 20;
-    var Yoffset = 40;
-
-    function handler(event) {
+    function handler(event, config) {
         minetip = event.currentTarget.querySelector("div");
         if (event.type == "mousemove" || event.type == "mouseover") {
             minetip.style.display = "block";
-            if (window.innerWidth < event.pageX + Xoffset + minetip.offsetWidth) {
-                minetip.style.left = event.pageX - Xoffset - minetip.offsetWidth + "px";
+            if (window.innerWidth < event.pageX + config.Xoffset + minetip.offsetWidth) {
+                minetip.style.left = event.pageX - config.Xoffset - minetip.offsetWidth + "px";
             } else {
-                minetip.style.left = event.pageX + Xoffset + "px";
+                minetip.style.left = event.pageX + config.Xoffset + "px";
             }
-            if (window.innerHeight < event.pageY - Yoffset + minetip.offsetHeight) {
+            if (window.innerHeight < event.pageY - config.Yoffset + minetip.offsetHeight) {
                 minetip.style.top = event.pageY - (event.pageY + minetip.offsetHeight - window.innerHeight) + "px";
             } else {
-                minetip.style.top = event.pageY - Yoffset + "px";
+                minetip.style.top = event.pageY - config.Yoffset + "px";
             }
         } else if (event.type == "mouseout") {
             minetip.style.display = "none";
         }
     }
 
+    // for (i = 0; i < config.length; i++) {
+    //     // let t = configs[i].getAttribute("data-img-width");
+    //     // console.log(t);
+    //     // t = configs[i].getAttribute("data-img-height");
+    //     // console.log(t);
+    //     // t = configs[i].getAttribute("data-img-alt");
+    //     // console.log(t);
+    // }
 
-    var minetips = document.querySelectorAll("span.minetip");
-    for (var i = 0; i < minetips.length; i++) {
-        minetips[i].addEventListener("mouseover", handler);
-        minetips[i].addEventListener("mouseout", handler);
-        minetips[i].addEventListener("mousemove", handler);
+    var i, j;
+    var minetip;
+    var default_config = {
+        load: {
+            Xoffset: 20,
+            Yoffset: 40,
+        },
+        event: {
+            Xoffset: 20,
+            Yoffset: 40,
+        },
+    };
+
+    var configs = document.querySelectorAll("span.minetips");
+    var minetips = configs.querySelectorAll("span.minetip");
+    var config;
+    for (i = 0; i < configs.length; i++) {
+        for (j = 0; j < minetips.length; j++) {
+            minetips[j].addEventListener("mouseover", function (event) {
+                return handler(event, config.event);
+            });
+            minetips[j].addEventListener("mouseout", function (event) {
+                return handler(event, config.event);
+            });
+            minetips[j].addEventListener("mousemove", function (event) {
+                return handler(event, config.event);
+            });
+        }
     }
 
 });
