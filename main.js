fad565f00d71cb5136d8c5ebf2dc87bdfb4ccc9e
Markov Dmitry <markovdmitry99@ya.ru>
2020-06-07 15:43:33 +0300

    now i can create minetip in 1 line + cleaning

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 /*jshint esversion: 6 */
 window.addEventListener("load", function () {
 
-    const default_config_load_img = {
-        "data-img-width": "32px",
-        "data-img-height": "32px",
-        "data-img-alt": "*img*",
-        "data-img-src": "static/img/placeholder.png",
-    };
-    const default_config_event = {
-        "data-offset-x": 20,
-        "data-offset-y": 40,
-    };
-
+    /*---FUNCTIONS---*/
     function string2element(string) {
         let d = document.createElement('div');
         d.innerHTML = string;
         return d.firstChild;
     }
 
-    function load_template(template_string, parameters_object) {
+    function apply_template(template_string, parameters_object) {
         for (const k in parameters_object) {
             if (parameters_object.hasOwnProperty(k)) {
                 template_string = template_string.replaceAll("{{k}}".replace("k", k), parameters_object[k]);
@@ -90,71 +80,65 @@ window.addEventListener("load", function () {
         }
     }
 
-    // let obsidian_sword_item = '<span class="minetip"> <img width="64px" height="64px" src="wip/obsidian_sword_item.png"><div> <span>Obsidian Sword</span><br><br> <span style="color: #A8A8A8; text-shadow: 0.11em 0.11em #292929;">When in main hand:</span><br> <span style="color: #00A800; text-shadow: 0.11em 0.11em #002900;">&nbsp;1.6 Attack Speed</span><br> <span style="color: #00A800; text-shadow: 0.11em 0.11em #002900;">&nbsp;6 Attack Damage</span><br> <span>Durability: 2341/2341</span><br> <span style="color: #545454; text-shadow: 0.11em 0.11em #151515;">obsidianstuff:obsidian_sword_item</span><br></div> </span>';
-    // console.log(string2element(obsidian_sword_item));
-
-    var template = {
-        sword: '<span class="minetip"> <img src="{{icon}}"><div> <span>{{name}}</span><br><br> <span style="color: #A8A8A8; text-shadow: 0.11em 0.11em #292929;">When in main hand:</span><br> <span style="color: #00A800; text-shadow: 0.11em 0.11em #002900;">&nbsp;{{attackspeed}} Attack Speed</span><br> <span style="color: #00A800; text-shadow: 0.11em 0.11em #002900;">&nbsp;{{attackdamage}} Attack Damage</span><br> <span>Durability: {{durability}}/{{durability}}</span><br> <span style="color: #545454; text-shadow: 0.11em 0.11em #151515;">{{id}}</span><br></div> </span>',
-    };
-    var item = {
-        obsidian_sword_item: {
-            icon: "wip/obsidian_sword_item.png",
-            name: "Obsidian Sword",
-            attackspeed: "1.6",
-            attackdamage: "6",
-            durability: "2341",
-            id: "obsidianstuff:obsidian_sword_item",
-        },
-    };
-    
-    let test = load_template(template.sword, item.obsidian_sword_item);
-    document.body.append(test);
-    console.log(test);
-    setup_element(test);
-
-    setup();
-});
-
-
-/*
-
-    let configs = document.querySelectorAll("span.minetips");
-    for (let a = 0; a < configs.length; a++) {
-        let minetips = configs[a].querySelectorAll("span.minetip");
-        for (let b = 0; b < minetips.length; b++) {
-            let config_attributes = minetips[b].parentElement.attributes;
-            let minetip_images = minetips[b].querySelectorAll("img");
-            let config_event = {};
-            for (let c in default_config_event) {
-                if (config_attributes.hasOwnProperty(c)) {
-                    config_event[c] = config_attributes[c].value;
-                } else {
-                    config_event[c] = default_config_event[c];
-                }
-            }
-            for (let c = 0; c < minetip_images.length; c++) {
-                let image_attributes = minetip_images[c].attributes;
-                let config_load_img = {};
-                for (let d in default_config_load_img) {
-                    if (image_attributes.hasOwnProperty(d.replace("data-img-", ""))) {
-                        config_load_img[d] = image_attributes[d.replace("data-img-", "")].value;
-                    } else if (config_attributes.hasOwnProperty(d)) {
-                        config_load_img[d] = config_attributes[d].value;
-                    } else {
-                        config_load_img[d] = default_config_load_img[d];
-                    }
-                }
-                for (let d in config_load_img) {
-                    if (config_load_img.hasOwnProperty(d)) {
-                        minetip_images[c].setAttribute(d.replace("data-img-", ""), config_load_img[d]);
-                    }
-                }
-            }
-            let handler = create_handler(config_event);
-            minetips[b].addEventListener("mouseover", handler);
-            minetips[b].addEventListener("mouseout", handler);
-            minetips[b].addEventListener("mousemove", handler);
+    function create_minetip(item) {
+        if (template.hasOwnProperty(item.type)) {
+            let e = apply_template(template[item.type], item.properties);
+            setup_element(e);
+            return e;
         }
     }
 
-*/
+    /*---DEFAULT-SETTINGS---*/
+    const default_config_load_img = {
+        "data-img-width": "32px",
+        "data-img-height": "32px",
+        "data-img-alt": "*img*",
+        "data-img-src": "static/img/placeholder.png",
+    };
+    const default_config_event = {
+        "data-offset-x": 20,
+        "data-offset-y": 40,
+    };
+
+    /*---TEMPLATES---*/
+    // Aka item types
+    const template = {
+        sword: '<span class="minetip"> <img src="{{icon}}"><div> <span>{{name}}</span><br><br> <span style="color: #A8A8A8; text-shadow: 0.11em 0.11em #292929;">When in main hand:</span><br> <span style="color: #00A800; text-shadow: 0.11em 0.11em #002900;">&nbsp;{{attackspeed}} Attack Speed</span><br> <span style="color: #00A800; text-shadow: 0.11em 0.11em #002900;">&nbsp;{{attackdamage}} Attack Damage</span><br> <span>Durability: {{durability}}/{{durability}}</span><br> <span style="color: #545454; text-shadow: 0.11em 0.11em #151515;">{{id}}</span><br></div> </span>',
+    };
+
+    /*---ITEMS---*/
+    const item = {
+        obsidian_sword_item: {
+            type: "sword",
+            properties: {
+                icon: "wip/obsidian_sword_item.png",
+                name: "Obsidian Sword",
+                attackspeed: "1.6",
+                attackdamage: "6",
+                durability: "2341",
+                id: "obsidianstuff:obsidian_sword_item",
+            },
+        },
+        /*
+        my_sword: {
+            type: "sword", //template
+            properties: {
+                icon: "my_sword.png", //img src
+                name: "Obsidian Sword",
+                attackspeed: "1.6",
+                attackdamage: "6",
+                durability: "2341",
+                id: "mod_id:my_sword",
+            },
+        },
+        */
+    };
+
+    let t = create_minetip(item.obsidian_sword_item);
+    document.body.append(t);
+    // console.log(t);
+
+    // <span class="minetips" data-offset-x="20" data-offset-y="40" data-img-width="32px" data-img-height="32px"
+    // data-img-alt="*image*" data-img-src="wip/obsidian_sword_item.png">
+    setup();
+});
